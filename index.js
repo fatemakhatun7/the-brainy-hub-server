533cac292decd233f4fba05d8641f19f81d0f6cb
fatemakhatun7 <fatema.sprdnc@gmail.com>
2022-10-25 17:47:00 +0600

    categories-loaded

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require("cors");
 app.use(cors());
 
 const Port = process.env.Port || 5000;
+const categories = require('./data/categories.json');
 
-app.get("/", (req, res) => {
-  res.send("Now server is running");
+app.get('/', (req, res) => {
+  res.send(categories)
 });
 
 app.listen(Port, () => {
